c0cf9874e12831de87ccc29904fd27ae4d14d225
seanoshea <oshea.ie@gmail.com>
2015-09-23 12:01:48 +0100

    Ignore seeds if running unit tests.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
+return if Rails.env.test?
+
 ScoreType.create! title: "Try", url: "", points: 5
 ScoreType.create! title: "Penalty Try", url: "", points: 5
 ScoreType.create! title: "Conversion", url: "", points: 2
