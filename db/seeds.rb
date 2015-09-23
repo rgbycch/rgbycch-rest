6cfe4bf14686a4e5033bc4ca670fcf9c5606eb51
seanoshea <oshea.ie@gmail.com>
2015-09-23 12:27:05 +0100

    Not running in test mode.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-return if Rails.env.test?
-
-ScoreType.create! title: "Try", url: "", points: 5
-ScoreType.create! title: "Penalty Try", url: "", points: 5
-ScoreType.create! title: "Conversion", url: "", points: 2
-ScoreType.create! title: "Penalty", url: "", points: 3
-ScoreType.create! title: "Drop Goal", url: "", points: 3
+if Rails.env.development? || Rails.env.production?
+  ScoreType.create! title: "Try", url: "", points: 5
+  ScoreType.create! title: "Penalty Try", url: "", points: 5
+  ScoreType.create! title: "Conversion", url: "", points: 2
+  ScoreType.create! title: "Penalty", url: "", points: 3
+  ScoreType.create! title: "Drop Goal", url: "", points: 3
+end
