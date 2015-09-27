cf46d3819720d0692c84b5ad083fc4af48f26a4c
seanoshea <oshea.ie@gmail.com>
2015-09-27 21:39:31 +0100

    Clubs and teams dependencies.

--- app/models/club.rb
+++ app/models/club.rb
@@ -4,7 +4,7 @@
 class Club < ActiveRecord::Base
   extend Searchable
   validates :title, presence: true
-  has_many :teams
+  has_many :teams, dependent: :destroy
   scope :filter_by_title, lambda { |keyword| where("lower(title) LIKE ?", "%#{keyword.downcase}%" ) }
 
   def self.id_params_identifier
--- spec/models/club_spec.rb
+++ spec/models/club_spec.rb
@@ -15,6 +15,24 @@ describe Club do
 
   it { should be_valid }
 
+  describe "#team association" do
+
+    before do
+      @club.save
+      3.times { FactoryGirl.create :team, club: @club }
+    end
+
+    it "destroys the associated teams on self destruct" do
+      teams = @club.teams
+      @club.destroy
+      teams.each do |team|
+        expect(Team.find(team.id)).to raise_error ActiveRecord::RecordNotFound
+      end
+
+    end
+
+  end
+
   context "filtering and searching" do
 
     before(:each) do
