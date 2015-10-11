059f353559faf6e94fe4d30ffc0de2d9e6e27063
seanoshea <oshea.ie@gmail.com>
2015-10-11 10:45:04 +0100

    This logic could live in the concern instead of creating a monster inheritance tree.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,20 +8,4 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
 
-  def failed_to_create(model, model_name)
-    logger.warn("Creating a new #{model_name} failed with #{model.errors.full_messages}")
-    render_error(model)
-  end
-
-  def failed_to_update(model, model_name)
-    logger.warn("Updating an existing #{model_name} failed with #{model.errors.full_messages}")
-    render_error(model)
-  end
-
-  private
-
-  def render_error(model)
-    render json: { errors: model.errors }, status: 422
-  end
-
 end
--- app/controllers/concerns/errorable.rb
+++ app/controllers/concerns/errorable.rb
@@ -12,4 +12,29 @@ module Errorable
     render json: "{\"error\":\"Record Not Found\"}", status: 404
   end
 
+  ##
+  # Used whenever a user tries to create a new record, but either validation of the record fails or the db is down.
+
+  def failed_to_create(model, model_name)
+    logger.warn("Creating a new #{model_name} failed with #{model.errors.full_messages}")
+    render_error(model)
+  end
+
+  ##
+  # Used whenever a user tries to update an existing record, but either validation of the record fails or the db is down.
+
+  def failed_to_update(model, model_name)
+    logger.warn("Updating an existing #{model_name} failed with #{model.errors.full_messages}")
+    render_error(model)
+  end
+
+  private
+
+  ##
+  # Renders the error back to the user with the appropriate status code.
+
+  def render_error(model)
+    render json: { errors: model.errors }, status: 422
+  end
+
 end
