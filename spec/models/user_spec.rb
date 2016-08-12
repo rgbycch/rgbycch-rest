4e8f21b3d545f813dceafe718e577aa4d07fe9bd
seanoshea <oshea.ie@gmail.com>
2016-08-11 23:13:27 -0400

    Per issue #40 - Final fixes.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,8 +11,7 @@ describe User, type: :model do
   it { should respond_to(:auth_token) }
   it { should be_valid }
 
-  it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email) }
+  it { should validate_presence_of(:email) }  
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
   it { should validate_uniqueness_of(:auth_token)}
