36a8e961ff8bdc599d79f94d04cd969cc767b9b0
seanoshea <oshea.ie@gmail.com>
2016-09-06 22:29:03 -0400

    Per issue #40 - Seeing if explicit up/downs work.

--- db/migrate/20150912163651_add_authentication_token_to_users.rb
+++ db/migrate/20150912163651_add_authentication_token_to_users.rb
@@ -1,6 +1,11 @@
 class AddAuthenticationTokenToUsers < ActiveRecord::Migration[4.2]
-  def change
+  def up
     add_column :users, :auth_token, :string, default: ""
     add_index :users, :auth_token, unique: true
+  end
+
+  def down
+    remove_column :users, :auth_token
+    remove_index :users, :auth_token
   end
 end
